5858f12c03dfca4db747d223d84ef6b92da8395f
Ezequiel Cruz Collazo <Cruzezequiel81@gmail.com>
2024-04-29 20:41:21 -0400

    Re do css

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
+/*const xClass = 'x'
+const circleClass = 'circle'
 let gridItems = document.querySelectorAll("[data-cell]");
+let circleTurn;
 
-gridItems.forEach(cell => {
-    cell.addEventListener('click', handleClick, {
+gridItems.forEach(item => {
+    item.addEventListener('click', handleClick, {
         once:true
     })
 });
 
 function handleClick(){
-    console.log('clicked');
+    const item = e.target;
+    const currentClass = circleTurn ? circleClass : xClass;
+    placeMark(item, currentClass)
 }
+
+function placeMark(item, currentClass){
+    item.classList.add(currentClass);
+}*/
